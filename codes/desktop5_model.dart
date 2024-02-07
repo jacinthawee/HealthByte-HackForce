34e16b22dfff932e7a4092ff80e4caa7548b96cf
Jacintha Wee <78328036+jacinthawee@users.noreply.github.com>
2024-02-07 21:06:56 +0800

    Update desktop5_model.dart

--- codes/desktop5_model.dart
+++ codes/desktop5_model.dart
@@ -3,7 +3,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/form_field_controller.dart';
-import 'desktop5_widget.dart' show Desktop5Widget;
+import 'desktop5.dart' show Desktop5Widget;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
