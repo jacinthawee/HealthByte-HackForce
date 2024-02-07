93cc4a92ddd2349a3cc0c2706efcd2ea72edbdd7
Jacintha Wee <78328036+jacinthawee@users.noreply.github.com>
2024-02-07 21:06:28 +0800

    Update desktop2_model.dart

--- codes/desktop2_model.dart
+++ codes/desktop2_model.dart
@@ -1,7 +1,7 @@
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'desktop2_widget.dart' show Desktop2Widget;
+import 'desktop2.dart' show Desktop2Widget;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
